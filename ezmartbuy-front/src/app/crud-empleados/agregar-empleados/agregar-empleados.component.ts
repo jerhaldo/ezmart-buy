5033f670e7d13a00ca4768868cd04e439f15a40b
Jerhaldo <eel_jerhal@hotmail.com>
2021-09-29 02:45:26 -0300

    + Funcionalidad de agregar empleados

--- ezmartbuy-front/src/app/crud-empleados/agregar-empleados/agregar-empleados.component.ts
+++ ezmartbuy-front/src/app/crud-empleados/agregar-empleados/agregar-empleados.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ContratoEmpleado, EmpleadosService } from 'app/services/empleados.service';
 
 @Component({
   selector: 'app-agregar-empleados',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar-empleados.component.css']
 })
 export class AgregarEmpleadosComponent implements OnInit {
+  
+  contrato_empleado: ContratoEmpleado= {
+    id_tienda: '',
+    rut_usuario: ''
+  };
 
-  constructor() { }
+  constructor(private empleadoService: EmpleadosService, private router: Router) { }
 
   ngOnInit(): void {
+  
   }
 
+  contratarEmpleado(){
+    this.contrato_empleado.id_tienda = '2';   //ID TIENDA
+    this.empleadoService.addEmpleado(this.contrato_empleado).subscribe(
+      res=>{
+        console.log(res);
+      },err=>{
+        console.log(err)
+      }
+    );
+
+    //this.router.navigate(['/productos']);
+    
+  }
+  
 }
