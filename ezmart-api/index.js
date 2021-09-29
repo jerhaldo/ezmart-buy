167c196298b27cb83255d9c5d0e7496e11fbe06a
Jerhaldo <eel_jerhal@hotmail.com>
2021-09-28 22:45:39 -0300

    + API empleados

--- ezmart-api/index.js
+++ ezmart-api/index.js
@@ -20,6 +20,7 @@ app.use('/api/', require('./routes_client'))            //RUTAS PARA LA TIENDA
 app.use('/api/products', require('./routes_products'))            //RUTAS PARA LA productos
 app.use('/api/category', require('./routes_category'))            //RUTAS PARA LA CATEGORIAS
 app.use('/api/upload', require('./routes_upload'))            //RUTAS PARA LA CATEGORIAS
+app.use('/api/employees', require('./routes_employees'))            //RUTAS PARA LOS EMPLEADOS
 app.use(express.static('public/data/uploads'));  
 
 
