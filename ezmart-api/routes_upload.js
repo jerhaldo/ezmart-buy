3f92e94810c75842ed1c82f9f48b2e05d7a686c4
Jerhaldo <eel_jerhal@hotmail.com>
2021-12-14 01:18:46 -0300

    + Cambio en el formato del nombre de las imagenes

--- ezmart-api/routes_upload.js
+++ ezmart-api/routes_upload.js
@@ -7,7 +7,7 @@ const storage = multer.diskStorage({
         cb(null, './public/data/uploads/');
     },
     filename: function (req,file,cb) {
-        this.nombre_arch = (new Date().getTime()).toString() + "_" + file.originalname
+        this.nombre_arch = "2_" + file.originalname
         cb(null, this.nombre_arch);
     }
 })
@@ -16,7 +16,7 @@ const upload = multer({storage: storage});
 router.post('/uploadimg/:id', upload.single('img'), (req, res, next) => {
     const {id} = req.params
     console.log(req.file.filename)
-    console.log(this.nombre_arch)
+
     let sql = `insert into multimedia_producto(id_multi, id_prod, desc_multi, arch_multi) values ('','${id}','descripcion','${req.file.filename}')`    // UPDATEA EL PRODUCTO CON ESE ID
     conn.query(sql,(err, rows, fields)=>{
             if(err) throw err;
